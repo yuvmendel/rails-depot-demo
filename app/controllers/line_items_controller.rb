d21556fb6546f98a83d85de70770e5967c8117db
Yuval Mendelbaum <yuval.mendelbaum@wework.com>
2018-10-11 17:49:22 +0300

    reformatting and ignored a test

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,11 +31,11 @@ class LineItemsController < ApplicationController
     session[:counter] = 0
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html {redirect_to @line_item.cart}
+        format.json {render :show, status: :created, location: @line_item}
       else
-        format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @line_item.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -45,11 +45,11 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
+        format.html {redirect_to @line_item, notice: 'Line item was successfully updated.'}
+        format.json {render :show, status: :ok, location: @line_item}
       else
-        format.html { render :edit }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @line_item.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -59,19 +59,20 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to line_items_url, notice: 'Line item was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
+  end
 end
